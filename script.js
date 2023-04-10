f92c5a370d9ce1412546babca75b13bef1ffba15
Pankaj kumar <pk497243@gmail.com>
2023-04-10 10:48:17 +0530

    file transfer with nice UI

--- script.js
+++ script.js
@@ -28,38 +28,38 @@ btn.addEventListener("click", () => {
 
   //   to send the message
   send.addEventListener("click", async () => {
-    // to send msg
-    // const msgvalue = msg.value;
-    // console.log(msgvalue);
-    // conn.send(msgvalue);
-    // console.log("message sent");
-
-    // to send file
-    // let [filehandle] = await window.showOpenFilePicker();
-    // let file = await filehandle.getFile();
-    // console.log(file);
-    // conn.send(file);
-
     // to send file to the other peer
     const file = document.getElementById("file").files[0];
     console.log(file);
     conn.send(file);
     console.log("file sent");
+    const filesent = document.getElementById("filesent");
+    console.log(filesent)
+    filesent.classList.remove("display_none");
+  
   });
 
   //   this will be executed when the connection is established
   conn.on("open", () => {
-    conn.send("Hello World! welcome to the first user");
+    console.log("we are successfully connected to the other peer");
+    const connected = document.getElementById("icon_done");
+    console.log(connected)
+    connected.classList.remove("display_none");
   });
 
   //   to recieve the data from the other peer
   conn.on("data", (data) => {
     console.log("Received data", data);
-    const fileReader = new FileReader();
-    fileReader.onload = () => {
-      console.log(fileReader.result);
-      console.log("file recieved successfully");
-    };
+    // we will use the concept of blob to download the file
+    const blob = new Blob([data], { type: "octet/stream" });
+    const url = window.URL.createObjectURL(blob);
+    const a = Object.assign(document.createElement("a"), {
+      href: url,
+      download: "file",
+    });
+    a.click();
+    window.URL.revokeObjectURL(url);
+    a.remove();
   });
 });
 
@@ -71,20 +71,14 @@ peer.on("connection", (conn) => {
   const msg = document.getElementById("msg");
   const send = document.getElementById("send");
 
-  //   to send the message to the other peer
-  send.addEventListener("click", () => {
-    const msgvalue = msg.value;
-    console.log(msgvalue);
-    conn.send(msgvalue);
-    console.log("message sent");
-  });
-
-  //   to recieve the data from the other peer
-  conn.on("data", (data) => {
-    conn.send("i am in the reciever side");
-    console.log("Received the data", data);
-
+  conn.on("data", function (data) {
     // to download the upcoming file
+    // what we have did here is that we have used the blob to store the data
+    // and then we have created a url for the blob so that we can download the data
+    // and then we have created a tag and then we have assigned the url to the href attribute
+    // and then we have assigned the download attribute to the tag
+    // and then we have clicked the tag 😀 so that the file will be downloaded
+    // and then we have revoked the url and then we have removed the tag
     const blob = new Blob([data], { type: "octet/stream" });
     const url = window.URL.createObjectURL(blob);
     const a = Object.assign(document.createElement("a"), {
@@ -94,6 +88,20 @@ peer.on("connection", (conn) => {
     a.click();
     window.URL.revokeObjectURL(url);
     a.remove();
+  });
+
+  //   to send the message to the other peer
+  send.addEventListener("click", () => {
+    const file = document.getElementById("file").files[0];
+    console.log(file);
+    conn.send(file);
+    console.log("file sent");
+
+    // to display that the file is sent
+    const filesent = document.getElementById("filesent");
+    console.log(filesent)
+    filesent.classList.remove("display_none");
+
   });
 });
 
